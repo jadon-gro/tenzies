e00f273f8faa0a5b0af87f05699e15db14147f2e
jadon-gro <jadon.gro@gatech.edu>
2023-01-25 13:49:00 -0500

    finished basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,57 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Die from "./components/Die"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
-  function randomDiceValue() { return Math.floor(Math.random() * 10) }
-  let [dice, setDice] = useState([
-    {
-      id: 0,
-      num: randomDiceValue(),
-      clicked: false
-    },
-    {
-      id: 1,
-      num: randomDiceValue(),
-      clicked: false
-    },
-    {
-      id: 2,
-      num: randomDiceValue(),
-      clicked: false
-    },
-    {
-      id: 3,
-      num: randomDiceValue(),
-      clicked: false
-    },
-    {
-      id: 4,
-      num: randomDiceValue(),
-      clicked: false
-    },
-    {
-      id: 5,
-      num: randomDiceValue(),
-      clicked: false
-    },
-    {
-      id: 6,
-      num: randomDiceValue(),
-      clicked: false
-    },
-    {
-      id: 7,
-      num: randomDiceValue(),
-      clicked: false
-    }
-  ])
+  function randomDiceValue() { return Math.ceil(Math.random() * 6) }
+  let [dice, setDice] = useState(initDice()) 
 
-  let diceComponents = dice.map((die) => <Die key={die.id} num={die.num} clicked={die.clicked}/>)
+  let [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const val = dice[0].num
+    let won = true
+    for (const die of dice) {
+      if (die.num !== val || !die.clicked) {
+        won = false;
+        break;
+      }
+    }
+    setTenzies(won)
+  }, [dice])
+
+  function initDice() {
+    let firstDice = []
+    for (let i = 0; i < 10; i++) {
+      firstDice.push({
+        id: nanoid(),
+        clicked: false,
+        num: randomDiceValue()
+      })
+    }
+    return firstDice
+  }
+  function reroll() {
+    if (tenzies) setDice(initDice())
+    else setDice(prevDice => prevDice.map(die => die.clicked ? die : {...die, num: randomDiceValue()}))
+  }
+
+  function clickDie(id) {
+    setDice(prevDice => prevDice.map(die => die.id === id ? {...die, clicked: !die.clicked} : die))
+  }
+
+  let diceComponents = dice.map((die) => <Die key={die.id} id={die.id} num={die.num} clicked={die.clicked} handleClick={() => clickDie(die.id)}/>)
+
   return(
     <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <h3 className="manual">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
       <div className="dice">
         {diceComponents}
       </div>
+      <button className="roll-button" onClick={reroll}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 
 export default function Die(props) {
   return(
-    <div className={`die {props.clicked ? "clicked" : ""}`}>
+    <div className={`die ${props.clicked ? "clicked" : ""}`} onClick={props.handleClick}>
       <p className="die--number">{props.num}</p>
     </div>
   )
